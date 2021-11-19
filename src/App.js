78fdd76a6ffd2023bd0c86f6da06ae5a87b12bb4
Nikolas Escobal <niko.escobal@gmail.com>
2021-11-20 05:33:02 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    // eslint-disable-next-line react/jsx-no-duplicate-props
+    <div className="App" className="p-6 items-center justify-center">
+      <h1 className="text-blue-400 font-extrabold"> Hello World! </h1>
+      {' '}
+      <p className="tracking-widest"> This is my first React App. </p>
+      {' '}
+    </div>
+  );
+}
+
+export default App;
