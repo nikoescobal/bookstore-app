c83cf234667c89bdd99b2978aa7673f92c139fa1
Niko Escobal <niko.escobal@gmail.com>
2021-11-20 18:03:03 +0800

    Update index.js and BookList.js component :

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
 import BookStore from './components/BookStore';
+import store from './redux/configureStore';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BookStore />
+    <Provider store={store}>
+      <BookStore />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
