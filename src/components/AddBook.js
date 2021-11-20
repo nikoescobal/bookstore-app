dcb0d5a80f3e8ab749d5714f36c60cf9d2faed5b
Niko Escobal <niko.escobal@gmail.com>
2021-11-21 00:37:33 +0800

    Update styling for AddBook component

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import '../styles/addbook.css';
 import { v4 as uuidv4 } from 'uuid';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../redux/books/books';
@@ -37,27 +38,30 @@ function AddBook() {
   return (
     <>
       <form action="#" onSubmit={handleSubmit}>
-        <h2>ADD NEW BOOK</h2>
-        <input
-          type="text"
-          placeholder="Book Title .."
-          onChange={(e) => handleTitleChange(e)}
-        />
-        <input
-          className="book-input"
-          placeholder="Author"
-          onChange={(e) => handleAuthorChange(e)}
-        />
-        <select id="books" name="books">
-          <option value="Fantasy">Fantasy</option>
-          <option value="Horror">Horror</option>
-          <option value="Drama">Drama</option>
-          <option value="Cooking">Cooking</option>
-        </select>
+        <h2 className="adding-span">ADD NEW BOOK</h2>
+        <div className="adding-form d-flex">
+          <input
+            className="book-input"
+            type="text"
+            placeholder="Book Title .."
+            onChange={(e) => handleTitleChange(e)}
+          />
+          <input
+            className="book-input"
+            placeholder="Author"
+            onChange={(e) => handleAuthorChange(e)}
+          />
+          <select className="selector-input" id="books" name="books">
+            <option value="Fantasy">Fantasy</option>
+            <option value="Horror">Horror</option>
+            <option value="Drama">Drama</option>
+            <option value="Cooking">Cooking</option>
+          </select>
+          <button type="button" onClick={(e) => submitBookToStore(e)}>
+            ADD BOOK
+          </button>
+        </div>
       </form>
-      <button type="button" onClick={(e) => submitBookToStore(e)}>
-        ADD BOOK
-      </button>
     </>
   );
 }
