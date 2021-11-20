ad3ed927b6b6a0e318600d992e2bc11238e065c1
Niko Escobal <niko.escobal@gmail.com>
2021-11-20 17:52:00 +0800

    Update package.json

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fill: (theme) => ({
+      red: theme('colors.red.500'),
+      green: theme('colors.green.500'),
+      blue: theme('colors.blue.500'),
+    }),
+    extend: {
+      fontFamily: {
+        nunito: ['"Nunito"', 'sans-serif'],
+        italianno: ['"Italianno"', 'cursive'],
+        raleway: ['"Raleway"', 'sans-serif'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
