0323a656e678a8166d4b628df1fc3f5665455be3
Niko Escobal <niko.escobal@gmail.com>
2021-11-21 00:49:14 +0800

    Update styling for nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/Nav.css';
+import userImg from '../assets/user.svg';
 
 function Nav() {
   return (
-    <div>
-      <ul>
-        <Link style={{ color: 'mintcream', fontSize: '26px' }} key="1" to="/">
-          Books &nbsp;
-        </Link>
-        <Link style={{ color: 'mintcream', fontSize: '26px' }} key="2" to="/categories">
-          Categories &nbsp;
-        </Link>
-      </ul>
-    </div>
+    <nav className="Nav">
+      <div className="left-side">
+        <h2 className="website-logo-name">Bookstore CMS</h2>
+        <ul>
+          <li>
+            <Link
+              className="links"
+              activeClassName="activelinks"
+              key="1"
+              to="/"
+            >
+              Books
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="links"
+              activeClassName="activelinks"
+              key="2"
+              to="/categories"
+            >
+              Categories
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="right-side">
+        <img src={userImg} alt="user-img" />
+      </div>
+    </nav>
   );
 }
 
